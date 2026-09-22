d59a1384a5f9640b182537ffd10b89b28e42c8f0
saimfayyaz-coder <saim.fayyaz@nextgeni.com>
2026-09-22 17:36:03 +0500

    Shared Configs setup and used in BaseAPI

--- src/shared/config/env.ts
+++ src/shared/config/env.ts
@@ -1,3 +1,19 @@
+import Config from 'react-native-config';
+
+export const config = {
+  appEnv: Config.APP_ENV || 'development',
+  appName: Config.APP_NAME || 'Instagram',
+  apiUrl: Config.API_URL || 'https://insta-backend-xi.vercel.app/api',
+  applicationId: Config.APPLICATION_ID || 'com.instagram',
+  isDev: Config.APP_ENV === 'development',
+  isStage: Config.APP_ENV === 'staging',
+  isProd: Config.APP_ENV === 'production',
+};
+
+export const BASE_URL = config.apiUrl;
+
 export const ENV = {
-  API_BASE_URL: 'https://api.instagram-clone-mock.com/api/v1',
-} as const;
+  API_BASE_URL: config.apiUrl,
+};
+
+export default config;
