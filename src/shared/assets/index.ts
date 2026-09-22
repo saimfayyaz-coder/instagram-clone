6ca6d41530909eb5a4d886be2e2b85cbfc336aed
saimfayyaz-coder <saim.fayyaz@nextgeni.com>
2026-09-22 16:39:27 +0500

    Bootsplash added

--- src/shared/assets/index.ts
+++
@@ -1,3 +0,0 @@
-export const instagramLogo = require('./logo/Instagram_Glyph_Gradient.svg');
-export const instagramAppIcon = require('./res/mipmap-xxhdpi/ic_launcher.png');
-export const instagramMonochrome = require('./res/mipmap-xxhdpi/ic_launcher_monochrome.png');
